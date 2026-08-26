5b2e48ad08a0607e096b4e236f63ec56ef8edd34
Aditya Dhadphale <contact.adityadhadphale@gmail.com>
2026-08-26 15:27:00 +0530

    DSA: update problem 01 - print 1 to 10

--- DSA/001_Loops/Q01_PRINT_1_TO_10.java
+++ DSA/001_Loops/Q01_PRINT_1_TO_10.java
@@ -1,10 +1,10 @@
 public class Q01_PRINT_1_TO_10 {
 
     public static void main (String [] args){
-        int a = 1;
-        while (a<=10){
-            System.out.println(a);
-            a++;
+        int counter = 1;
+        while (counter<=10){
+            System.out.println(counter);
+            counter++;
         }
     }
 }
